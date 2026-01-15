5f321e9b7562fb07d3ae8b525d01b8123fd36e1a
Anusha Akhter ❤️ <akhteranusha38@gmail.com>
2026-01-15 19:02:25 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
 let input = document.getElementById("data");
+let sound = new Audio('sound.mp3');
+let buttons = document.querySelectorAll('button');
+
+for(let i = 0; i < buttons.length; i++){
+    buttons[i].addEventListener('click' , ()=>{
+       sound.currentTime = 0;
+       sound.play();
+    });
+}
 
 function clearValue(){
     input.value="";
